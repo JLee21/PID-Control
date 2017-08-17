dfb782dfd6f069691c2bdaefe072eab42ac31c0b
Jordan Lee <topher@SnoopMac.local>
2017-08-16 21:18:08 -0700

    first commit

---
+++ src/PID.cpp
@@ -0,0 +1,43 @@
+#include "PID.h"
+
+using namespace std;
+
+/*
+* TODO: Complete the PID class.
+*/
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp, double Ki, double Kd) {
+
+  // similar to python's self.Kp = Kp
+  // this means that whatever class object called this Init() function,
+  //   assign that object's Kp,Ki,Kd the following:
+  this->Kp = Kp;
+  this->Ki = Ki;
+  this->Kd = Kd;
+
+  p_error = 0;
+  i_error = 0;
+  d_error = 0;
+
+  // printf("%s\n", "TEST");
+  // std::cout << 'Kp:' << Kp << " Ki:" << Ki << " Kd:" << Kd << endl;
+
+}
+
+void PID::UpdateError(double cte) {
+
+  d_error = cte - p_error;
+  p_error = cte;
+  i_error += cte;
+
+}
+
+double PID::TotalError() {
+
+  return Kp*p_error + Kd*d_error + Ki*i_error;
+
+}
